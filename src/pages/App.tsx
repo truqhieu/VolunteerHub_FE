c4955c7c6f29bcb339700782cbe0f6095691030d
Duc <vominhduc03ht@gmail.com>
2025-06-03 10:37:04 +0700

    HomeDetail

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
 import Register from '../pages/register/Register';
+import HomeDetail from './homedetail/HomeDetail';
 import './App.css';
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/detail" element={<HomeDetail />} />
         </Routes>
       </div>
     </BrowserRouter>
